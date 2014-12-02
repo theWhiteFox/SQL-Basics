b8f46a0f9d38e556da17d9105b0086694498b85e
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-02 11:58:43 +0000

    with Style

---
+++ SQL-basics/bookupdate.sql
@@ -0,0 +1,1 @@
+UPDATE `books` SET `author`='Luke Wroblewski', `title`='Mobile First', `link`='http://www.abookapart.com/products/mobile-first', `image`='images/mobile_first.jpg' WHERE  `book_id`=2;
