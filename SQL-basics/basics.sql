a638246c69001d01ca14c762ff182e6bf52bb226
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-30 13:07:57 +0000

    CMD basic MySql statements

---
+++ SQL-basics/basics.sql
@@ -0,0 +1,17 @@
+-- login to my local MySql server
+mysql -u root -p
+-- password: 
+SHOW databases;
+CREATE DATABASE db_name;
+USE db_name;
+SHOW tables;
+CREATE TABLE table_name (
+id int(10) NOT NULL AUTO_INCREMENT,
+foreign_key_id int(10),
+name varchar(255),
+date timestamp,
+PRIMARY KEY(id),
+FOREIGN KEY(foreign_key_id) REFERENCES table_name(foreign_key_id)
+);
+
+
